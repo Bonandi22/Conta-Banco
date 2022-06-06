d4890c7cb5c514ca7f61fe76c4a5a4ae75c98a86
Bonandi <elkerbonandi@gmail.com>
2022-06-06 18:57:25 +0100

    primeiro proj

---
+++ conta/ConsoleApp10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta contaP = new Conta();
+
+
+
+
+        }
+    }
+}
